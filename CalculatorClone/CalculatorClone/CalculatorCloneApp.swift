dbff65ef32f71209617fd5958f20c7c51736a33d
rich-eli <130540380+rich-eli@users.noreply.github.com>
2023-05-21 19:10:05 +0700

    Add files via upload

---
+++ CalculatorClone/CalculatorClone/CalculatorCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorCloneApp.swift
+//  CalculatorClone
+//
+//  Created by EL on 17/05/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
